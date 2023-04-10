d94fdbc7b5c9a656910b918264f739dc997e585b
Romain <mw3pumat8@gmail.com>
2023-04-11 00:17:21 +0200

    WIP interactions SQL

--- data/views/mySQL.php
+++ data/views/mySQL.php
@@ -2,13 +2,25 @@
 $host = 'mysql8'; //Nom donné dans le docker-compose.yml
 $user = 'myuser';
 $password = 'monpassword';
-$db = 'tutoseu';
+$db = 'db-MamaMia';
 
-$conn = new mysqli($host,$user,$password,$db);
-if(!$conn) {echo "Erreur de connexion à MSSQL<br />";}
-else{
-        echo "Connexion à MSSQL ok<br />";
-        mysqli_close($conn);
-}
+$mysqli = new mysqli($host, $user, $password, $db);
+
+$result = $mysqli->query("SELECT * FROM recipe WHERE id='1'");
+// $result = $mysqli->query("CREATE TABLE `db-MamaMia`.`recipe` (
+//                                                 `id` INT NOT NULL,
+//                                                 `name` VARCHAR(45) NULL,
+//                                                 `description` VARCHAR(255) NULL,
+//                                                 `category_id` INT NULL,
+//                                                 `steps` LONGTEXT NULL,
+//                                                 `duration` INT NULL,
+//                                                 `recipecol` VARCHAR(45) NULL,
+//                                                 PRIMARY KEY (`id`),
+//                                                 UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);"
+//         );
+$row = $result->fetch_assoc();
+echo $row['name'];
+echo '<br>';
+var_dump($row);
 
 ?>
